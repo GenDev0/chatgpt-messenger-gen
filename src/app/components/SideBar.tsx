dde88192f707fc0e10d15e53616a20823f184ceb
GenDev0 <dev.gen.info.0@gmail.com>
2023-03-12 16:15:36 +0100

    add chats useCollection on the SideBar

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,9 +1,19 @@
 "use client";
+import { collection } from "firebase/firestore";
 import { useSession, signOut } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../../firebase";
 import NewChat from "./NewChat";
 
 function SideBar() {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session && collection(db, "users", session.user?.email!, "chats")
+  );
+
+  console.log(chats);
+
   return (
     <div className='p-2 flex flex-col min-h-screen'>
       <div className='flex-1'>
