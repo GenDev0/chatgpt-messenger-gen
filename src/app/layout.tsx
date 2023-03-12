5ba33335c50a8f7f4dc417729b210ac0fba87d03
GenDev0 <dev.gen.info.0@gmail.com>
2023-03-12 12:17:15 +0100

    create Sesstion Provider:Types and import it in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
+import SessionProvider from "./components/SessionProvider";
 import SideBar from "./components/SideBar";
 import "./globals.css";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang='en'>
       <body>
-        <div className='flex'>
-          {/* SideBar */}
-          <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem] '>
-            <SideBar />
+        <SessionProvider session={session}>
+          <div className='flex'>
+            {/* SideBar */}
+            <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem] '>
+              <SideBar />
+            </div>
+            {/* ClientProvider - Notifications */}
+            <div className='bg-[#343541] flex-1'>{children}</div>
           </div>
-          {/* ClientProvider - Notifications */}
-          <div className='bg-[#343541] flex-1'>{children}</div>
-        </div>
+        </SessionProvider>
       </body>
     </html>
   );
